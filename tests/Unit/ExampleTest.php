1a68fe89af1c3ee729f2c67d0fdad53c55025195
Roman Kostrov <rkostrov@yandex.ru>
2025-04-05 12:33:31 +0300

    123

--- tests/Unit/ExampleTest.php
+++ tests/Unit/ExampleTest.php
@@ -6,16 +6,6 @@ use PHPUnit\Framework\TestCase;
 
 class ExampleTest extends TestCase
 {
-    /**
-     * A basic test example.
-     */
-    public function testThatTrueIsTrue(): void
-    {
-        $valami = true;
-        // dd($valami);
-        $this->assertTrue($valami);
-    }
-
     public function testSajatTest(): void
     {
         $valami = 2;
