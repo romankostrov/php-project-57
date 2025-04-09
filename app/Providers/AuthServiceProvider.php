0cf694758c593ac85d6269fe92b50f61c078f774
Roman Kostrov <rkostrov@yandex.ru>
2025-04-09 19:22:01 +0300

    new commit

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -2,10 +2,7 @@
 
 namespace App\Providers;
 
-use App\Models\TaskStatus;
-use App\Policies\TaskStatusPolicy;
-use App\Policies\LabelPolicy;
-
+// use Illuminate\Support\Facades\Gate;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
 
 class AuthServiceProvider extends ServiceProvider
@@ -16,7 +13,7 @@ class AuthServiceProvider extends ServiceProvider
      * @var array<class-string, class-string>
      */
     protected $policies = [
-        TaskStatus::class => TaskStatusPolicy::class
+        //
     ];
 
     /**
@@ -24,6 +21,6 @@ class AuthServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        $this->registerPolicies();
+        //
     }
 }
